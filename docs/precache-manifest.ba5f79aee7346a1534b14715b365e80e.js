6477ac7940daf91ccb1ed297648c9886bf6c27b0
GerCoss <gerardocoss92@gmail.com>
2020-06-23 19:32:17 -0500

    docs agregado

---
+++ docs/precache-manifest.ba5f79aee7346a1534b14715b365e80e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c68abe30968ef025b5c047935be7182",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2331eaf65d2a716266c",
+    "url": "/static/css/main.ae148714.chunk.css"
+  },
+  {
+    "revision": "51e34cff51b285543b30",
+    "url": "/static/js/2.04409896.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.04409896.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2331eaf65d2a716266c",
+    "url": "/static/js/main.bf32acf2.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
